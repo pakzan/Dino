ee122de6f6e7837369b6254150e7215b57cfaa9f
Pak Zan Tan <pakzzzan@hotmail.com>
2022-08-21 17:54:25 +0800

    Updates

---
+++ precache-manifest.21000ea559f2473e47b306b07fd154fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8da6864fe0952272a3c",
+    "url": "/dino-game/static/js/main.fde7e021.chunk.js"
+  },
+  {
+    "revision": "cb6f40b6d83163e7aa55",
+    "url": "/dino-game/static/js/runtime~main.f1c74179.js"
+  },
+  {
+    "revision": "1211c329c47a0a889d19",
+    "url": "/dino-game/static/js/2.51ac6db2.chunk.js"
+  },
+  {
+    "revision": "1bc85ac8d32c081c1b3a6580fefe0870",
+    "url": "/dino-game/index.html"
+  }
+];
